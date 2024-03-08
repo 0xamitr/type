fd168abef4b675335d6d56099497e84615dde149
0xamitr <0xamitr@gmail.com>
2024-03-08 21:48:27 +0530

    accuracy

--- src/app/components/text.tsx
+++ src/app/components/text.tsx
@@ -5,6 +5,7 @@ import { useEffect, useState } from "react";
 export default function Text(props: any) {
     const [wpm, setWPM] = useState(0)
     const [finish, setFinish] = useState(false)
+    const [wrong, setWrong] = useState(0)
 
     let time: number = 0
     let text: String = props.text;
@@ -13,6 +14,7 @@ export default function Text(props: any) {
     let timerID: any
     let start: boolean = false
     let temp: number = 0
+    let wrongvar: number = 0
 
     const startTime = () => {
         timerID = setInterval(() => {
@@ -23,36 +25,49 @@ export default function Text(props: any) {
         console.log("length", text.length)
         console.log(finish)
         const handleKeyDown = (event: any) => {
-
-                if (!start) {
-                    startTime()
-                    start = true
-                }
-                if (event.key == " ") {
-                    temp++
-                }
-                if (event.key == text[index]) {
-                    document.querySelector(".current")?.classList.remove("current")
-                    console.log(event.key)
-                    index++
-                    document.getElementById(`${index}`)?.classList.add("typed")
-                    document.getElementById(`${index+1}`)?.classList.add("current")
-                }
-                if (index == text.length) {
-                    console.log("ok")
-                    clearInterval(timerID)
-                    setFinish(true)
-                    props.fetchRandomText()
-                    temp++
-                    const n = temp / (time / 6000)
-                    setWPM(Math.floor(n * 100) / 100)
-                }
+            console.log(event.key)
+            if(event.key == "Escape"){
+                setFinish(true)
+            }
+            if(event.key == "Shift" || event.key == "CapsLock" || event.key == "Control"){
+                return
+            }
+            if (!start) {
+                startTime()
+                start = true
+            }
+            if (event.key == " ") {
+                temp++
+            }
+            if (event.key == text[index]) {
+                document.querySelector(".current")?.classList.remove("current")
+                console.log(event.key)
+                index++
+                document.getElementById(`${index}`)?.classList.add("typed")
+                document.getElementById(`${index+1}`)?.classList.add("current")
+            }
+            else{
+                console.log("wrong", event.key, text[index], index)
+                document.getElementById(`${index+1}`)?.classList.add("wrong")
+                wrongvar++;
+            }
+            if (index == text.length) {
+                console.log("ok")
+                clearInterval(timerID)
+                setFinish(true)
+                setWrong(wrongvar)
+                props.fetchRandomText()
+                temp++
+                const n = temp / (time / 6000)
+                setWPM(Math.floor(n * 100) / 100)
+            }
         }
         document.addEventListener('keydown', handleKeyDown);
         return () => {
+            console.log(":fsklajfkl")
             document.removeEventListener('keydown', handleKeyDown);
         };
-    })
+    }, [finish, text])
 
     return (
         <div>
@@ -68,7 +83,10 @@ export default function Text(props: any) {
                 })}
             </h2>
             {finish &&
-                <h2>{wpm} wpm</h2>
+                <>
+                    <h3>Speed: {wpm} wpm</h3>
+                    <h3>Accuracy: {Math.floor((100-((wrong/text.length)*100)) * 100) / 100}%</h3>
+                </>
             }
         </div>
     )
