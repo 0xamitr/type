c94ac539df877b1aac5f52efc42a038a453ec234
0xamitr <amitrathore.aaa@gmail.com>
2024-04-02 17:12:49 +0530

    token

--- mytype/app.py
+++ mytype/app.py
@@ -1,19 +1,25 @@
-from flask import Flask, request
+from flask import Flask, request, jsonify
 from flask_mysqldb import MySQL
 from flask_cors import CORS
 from database_operation import create_user
 from flask_bcrypt import Bcrypt
 
+from flask_jwt_extended import create_access_token
+from flask_jwt_extended import get_jwt_identity
+from flask_jwt_extended import jwt_required
+from flask_jwt_extended import JWTManager
 
 app = Flask(__name__)
 CORS(app)
 
+app.config["JWT_SECRET_KEY"] = "super-secret"
 app.config['MYSQL_USER'] = 'sql6695551'
 app.config['MYSQL_PASSWORD'] = 'qB9zge7eHz'
 app.config['MYSQL_HOST'] = 'sql6.freemysqlhosting.net' 
 app.config['MYSQL_DB'] = 'sql6695551'
 app.config['MYSQL_CURSORCLASS'] = 'DictCursor'
 
+jwt = JWTManager(app)
 mysql = MySQL(app)
 bcrypt = Bcrypt(app)
     
@@ -44,7 +50,6 @@ def insert():
 @app.route('/login', methods=['POST'])
 def check():
     try:
-        print("ok")
         data = request.get_json()
         email = data['email']
         password = data['password']
@@ -57,12 +62,12 @@ def check():
         if result:
             hashed_password = result['password']
             if bcrypt.check_password_hash(hashed_password, password):
-                print( 'User found')
+                access_token = create_access_token(identity=email)
+                return jsonify(access_token=access_token)
             else:
-                print( 'Incorrect password')
+                return jsonify({"msg": "Bad username or password"}), 401
         else:
-            print( 'User not found')
-        return "fa"
+            return jsonify({"msg": "Bad username or password"}), 401
     except Exception as e:
         return {'error': str(e)}, 500
 
